7846a462d21b1509a33082af79276fc31229d436
Antonio Ferrándiz <antoniojfb01@gmail.com>
2022-05-02 22:03:40 -0500

    features update

--- Go2Climb.API/Go2Climb.API.Tests/Steps/ContractServiceToAgencyDefinition.cs
+++ Go2Climb.API/Go2Climb.API.Tests/Steps/ContractServiceToAgencyDefinition.cs
@@ -18,57 +18,96 @@ namespace Go2Climb.API.Tests.Steps
         [Given(@"the tourist is on the home page")]
         public void GivenTheTouristIsOnTheHomePage()
         {
-            
+            // Abre el front
             webDriver.Navigate().GoToUrl("https://go2climb-6758a.web.app/");
-            Thread.Sleep(25000);
+            Thread.Sleep(5000);
 
+            // Busca y da click el menu tipo hamburguesa
+            webDriver.FindElement(By.XPath("/html/body/div[1]/div/header/div/div/div[2]/div/button[2]")).Click();
             
-            // webDriver.FindElement(By.XPath("//*[@id='app']/div[1]/header/div/div/div[2]/div/button[2]")).Click();
-            //
-            // //*[@id="list-item-98"]
-            // webDriver.FindElement(By.Id("list-item-48")).Click();
-            //
-            // webDriver.FindElement(By.XPath("//*[@id='input-59']")).Click();
-            // var emailInput = webDriver.FindElement(By.XPath("//*[@id='input-59']"));
-            // emailInput.SendKeys("cliente@gmail.com");
-            //
-            // webDriver.FindElement(By.XPath("//*[@id='input-62']")).Click();
-            // var passwordInput = webDriver.FindElement(By.XPath("//*[@id='input-62']"));
-            // passwordInput.SendKeys("Cliente1!");
-            //
-            // webDriver.FindElement(By.XPath("//*[@id='app']/div[4]/div/div/div[1]/form/button/span")).Click();
+            // Busca y da click al elemento de la lista "Sign In"
+            webDriver.FindElement(By.XPath("/html/body/div/div[2]/div/div[1]")).Click();
+
+            // Busca el input de Email 
+            var emailInput = webDriver.FindElement(By.XPath("/html/body/div/div[4]/div/div/div[1]/form/div[1]/div/div[1]/div/input"));
+            // Le da click al input de email
+            emailInput.Click();
+            // Escribe en el input el email
+            emailInput.SendKeys("cliente@gmail.com");
+            
+            // Busca el input de Password
+            var passwordInput = webDriver.FindElement(By.XPath("/html/body/div/div[4]/div/div/div[1]/form/div[2]/div/div[1]/div/input"));
+            // Le da click
+            passwordInput.Click();
+            // Ingresa la contraseña el input
+            passwordInput.SendKeys("Cliente1!");
+            
+            // Busca y da click en el boton Sign In
+            webDriver.FindElement(By.XPath("/html/body/div/div[4]/div/div/div[1]/form/button/span")).Click();
+            Thread.Sleep(5000);
         }
         
         
         [Given(@"and select the service you wish to contract")]
         public void GivenAndSelectTheServiceYouWishToContract()
         {
-            webDriver.FindElement(By.XPath("//*[@id='forYou']/div/div[2]/div/div[1]/div/a/div/div[3]")).Click();
+            // Busca y da click en un servicio que se encuentre disponible
+            webDriver.FindElement(By.XPath("/html/body/div/div[1]/main/div/div/div/div[2]/div[1]/div/div[2]/div/div/div")).Click();
         }
         
         [When(@"fills in the requested information to request the service")]
         public void WhenFillsInTheRequestedInformationToRequestTheService(Table table)
         {
-            webDriver.FindElement(By.Id("input-35")).Click();
-            var dateInput = webDriver.FindElement(By.Id("input-35"));
+            Thread.Sleep(5000);
+            // Busca y da click en el input de Output
+            var dateInput = webDriver.FindElement(By.XPath("/html/body/div/div[1]/main/div/div/div/div[1]/div/div/div[2]/form/div[2]/div/div/div/input"));
+            dateInput.Click();
             dateInput.SendKeys("05052022");
             
-            webDriver.FindElement(By.Id("input-37")).Click();
-            var persons = webDriver.FindElement(By.Id("input-37"));
+            // Busca y da click en el input de persons
+            var persons = webDriver.FindElement(By.XPath("/html/body/div[1]/div/main/div/div/div/div[1]/div/div/div[2]/form/div[4]/div/div/div/input"));
+            persons.Click();
             persons.SendKeys("5");
+            
+            // Busca y da click en el boton Solicit
+            webDriver.FindElement(By.XPath("/html/body/div/div[1]/main/div/div/div/div[1]/div/div/div[2]/div[1]/div/button")).Click();
+            
+            Thread.Sleep(2500);
+            
+            
+            var creditCard = webDriver.FindElement(By.XPath("/html/body/div/div[5]/div/div/div/form/div[2]/div/div/div/input"));
+            creditCard.Click();
+            creditCard.SendKeys("4556459834142584");
+            
+            
+            var nameOfTheAccountHolder = webDriver.FindElement(By.XPath("/html/body/div/div[5]/div/div/div/form/div[3]/div/div/div/input"));
+            nameOfTheAccountHolder.Click();
+            nameOfTheAccountHolder.SendKeys("Pedrito Sanchez");
+            
+            var creditCardDate = webDriver.FindElement(By.XPath("/html/body/div/div[5]/div/div/div/form/div[4]/div[1]/div/div/div/input"));
+            creditCardDate.Click();
+            creditCardDate.SendKeys("11/26");
+            
+            var creditCardCvv = webDriver.FindElement(By.XPath("/html/body/div/div[5]/div/div/div/form/div[4]/div[2]/div/div/div/input"));
+            creditCardCvv.Click();
+            creditCardCvv.SendKeys("631");
+            Thread.Sleep(2000);
+            
         }
 
         [Then(@"click on contract")]
         public void ThenClickOnContract()
         {
-            // Click en botón Solicit
-            webDriver.FindElement(By.XPath("//*[@id='8']/div/div/div/div[1]/div/div/div[2]/div[1]/div/button/span")).Click();
-
+            webDriver.FindElement(By.XPath("/html/body/div/div[5]/div/div/div/div[2]/button")).Click();
+            Thread.Sleep(2000);
+            // Click en ok
+            webDriver.FindElement(By.XPath("//*[@id='app']/div[7]/div/div/div/button[2]")).Click();
             // Pasos para cerrar sesión
             // Click en menu hamburguesa
-            webDriver.FindElement(By.XPath("//*[@id='app']/div[1]/header/div/div/div[2]/div/button[2]/span/i")).Click();
+            webDriver.FindElement(By.XPath("/html/body/div[1]/div/header/div/div/div[2]/div/button[2]")).Click();
             //Click en Log Out
-            webDriver.FindElement(By.XPath("//*[@id='list-item-71']")).Click();
+            Thread.Sleep(2000);
+            webDriver.FindElement(By.XPath("/html/body/div/div[2]/div/div[2]")).Click();
             webDriver.Close();
         }
 
--- Go2Climb.API/Go2Climb.API.Tests/Steps/ViewServiceValorationDefinition.cs
+++ Go2Climb.API/Go2Climb.API.Tests/Steps/ViewServiceValorationDefinition.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using System.Threading;
+using OpenQA.Selenium;
 using TechTalk.SpecFlow;
 
 namespace Go2Climb.API.Tests.Steps
@@ -16,22 +17,33 @@ namespace Go2Climb.API.Tests.Steps
         [Given(@"the tourist would like to see ratings from an agency")]
         public void GivenTheTouristWouldLikeToSeeRatingsFromAnAgency()
         {
+            // Abre el front
             webDriver.Navigate().GoToUrl("https://go2climb-6758a.web.app/");
-            
-            webDriver.FindElement(By.XPath("//*[@id='app']/div[1]/header/div/div/div[2]/div/button[2]")).Click();
-            
-            //*[@id="list-item-98"]
-            webDriver.FindElement(By.Id("list-item-48")).Click();
+            Thread.Sleep(5000);
 
-            webDriver.FindElement(By.XPath("//*[@id='input-59']")).Click();
-            var emailInput = webDriver.FindElement(By.XPath("//*[@id='input-59']"));
+            // Busca y da click el menu tipo hamburguesa
+            webDriver.FindElement(By.XPath("/html/body/div[1]/div/header/div/div/div[2]/div/button[2]")).Click();
+            
+            // Busca y da click al elemento de la lista "Sign In"
+            webDriver.FindElement(By.XPath("/html/body/div/div[2]/div/div[1]")).Click();
+
+            // Busca el input de Email 
+            var emailInput = webDriver.FindElement(By.XPath("/html/body/div/div[4]/div/div/div[1]/form/div[1]/div/div[1]/div/input"));
+            // Le da click al input de email
+            emailInput.Click();
+            // Escribe en el input el email
             emailInput.SendKeys("cliente@gmail.com");
             
-            webDriver.FindElement(By.XPath("//*[@id='input-62']")).Click();
-            var passwordInput = webDriver.FindElement(By.XPath("//*[@id='input-62']"));
+            // Busca el input de Password
+            var passwordInput = webDriver.FindElement(By.XPath("/html/body/div/div[4]/div/div/div[1]/form/div[2]/div/div[1]/div/input"));
+            // Le da click
+            passwordInput.Click();
+            // Ingresa la contraseña el input
             passwordInput.SendKeys("Cliente1!");
             
-            webDriver.FindElement(By.XPath("//*[@id='app']/div[4]/div/div/div[1]/form/button/span")).Click();
+            // Busca y da click en el boton Sign In
+            webDriver.FindElement(By.XPath("/html/body/div/div[4]/div/div/div[1]/form/button/span")).Click();
+            Thread.Sleep(5000);
             
             
         }
@@ -39,19 +51,20 @@ namespace Go2Climb.API.Tests.Steps
         [Given(@"click on the agency's website")]
         public void GivenClickOnTheAgencysWebsite()
         {
-            webDriver.FindElement(By.XPath("//*[@id='forYou']/div/div[2]/div/div[3]/div/a/div/div[3]")).Click();
-        }
+            // Busca y da click en un servicio que se encuentre disponible
+            webDriver.FindElement(By.XPath("/html/body/div/div[1]/main/div/div/div/div[2]/div[1]/div/div[2]/div/div/div")).Click();        }
 
         [Then(@"in the reviews section you will be able to visualize the valuation of the agency")]
         public void ThenInTheReviewsSectionYouWillBeAbleToVisualizeTheValuationOfTheAgency()
         {
-            webDriver.FindElement(By.XPath("//*[@id='24']/div/div/div/div[2]/div[1]/div/div[2]/div[2]/div"));
-            
+            Thread.Sleep(2000);
+            webDriver.FindElement(By.XPath("/html/body/div[1]/div/main/div/div/div/div[2]/div[3]")).Click();
+            Thread.Sleep(10000);
             // Pasos para cerrar sesión
             // Click en menu hamburguesa
             webDriver.FindElement(By.XPath("//*[@id='app']/div[1]/header/div/div/div[2]/div/button[2]/span/i")).Click();
             //Click en Log Out
-            webDriver.FindElement(By.XPath("//*[@id='list-item-71']")).Click();
+            webDriver.FindElement(By.XPath("/html/body/div/div[2]/div/div[2]")).Click();
             webDriver.Close();
         }
     }
