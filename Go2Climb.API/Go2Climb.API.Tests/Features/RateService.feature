170aafde387d0733dd80f16973514049759c9f5e
kimjongappreciator <nonexistentexistentialist1@gmail.com>
2022-05-05 00:38:59 -0500

    a

---
+++ Go2Climb.API/Go2Climb.API.Tests/Features/RateService.feature
@@ -0,0 +1,14 @@
+﻿Feature: RateService
+	As a client i want to rate a sevice
+
+@mytag
+Scenario: The service is finished
+	Given The client is logged
+	And enters to his profile 
+	When the client clicks on rate service
+	Then he will be able to rate the service
+Scenario: The service is not finished
+	Given The client is logged
+	And enters to his profile 
+	When the client clicks on rate service 2
+	Then he will not be able to rate the service
---
+++ Go2Climb.API/Go2Climb.API.Tests/Steps/RateServicedefinition.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace Go2Climb.API.Tests.Steps
+{
+    [Binding]
+    public class RateServicedefinition
+    {
+        public static WebDriver webdriver;
+
+        public RateServicedefinition()
+        {
+            webdriver = MyWebDriver.getWebdriver();
+        }
+        
+        [Given(@"The client is logged")]
+        public void GivenTheClientIsLogged()
+        {   
+            ScenarioContext.StepIsPending();
+        }
+
+        [Given(@"enters to his profile")]
+        public void GivenEntersToHisProfile()
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [When(@"the client clicks on rate service")]
+        public void WhenTheClientClicksOnRateService()
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Then(@"he will be able to rate the service")]
+        public void ThenHeWillBeAbleToRateTheService()
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [When(@"the client clicks on rate service (.*)")]
+        public void WhenTheClientClicksOnRateService(int p0)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Then(@"he will not be able to rate the service")]
+        public void ThenHeWillNotBeAbleToRateTheService()
+        {
+            ScenarioContext.StepIsPending();
+        }
+    }
+}
