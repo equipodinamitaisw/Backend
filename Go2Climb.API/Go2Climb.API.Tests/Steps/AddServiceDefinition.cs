545c4e4bd3ad2d17befafcce384157a1a2dfd2a6
Josmarvs <siriusjomsar123@gmail.com>
2022-05-05 00:36:39 -0500

    nuevos features

---
+++ Go2Climb.API/Go2Climb.API.Tests/Steps/AddServiceDefinition.cs
@@ -0,0 +1,14 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Go2Climb.API.Tests.Steps
+{
+    [Binding]
+    public class AddServiceDefinition
+    {
+        [Given(@"the agency wants to add new service")]
+        public void GivenTheAgencyWantsToAddNewService()
+        {
+            ScenarioContext.StepIsPending();
+        }
+    }
+}
